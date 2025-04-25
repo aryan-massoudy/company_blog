7decbe9815ed72db0c04fbe5044b5863eedc1384
ARYAN MASSOUDY <116915753+aryan-massoudy@users.noreply.github.com>
2025-04-25 09:15:35 +0200

    push from work PC

--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
         return self.title    
     
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post',related_name='comments')
+    post = models.ForeignKey('blog.Post',related_name='comments',on_delete=models.CASCADE)
     author = models.CharField(max_length=128)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
