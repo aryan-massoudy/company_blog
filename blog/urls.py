51d46fe5d3c3ac4dc5561dc68ffab7cd35f5a098
ARYAN MASSOUDY <116915753+aryan-massoudy@users.noreply.github.com>
2025-04-28 18:11:22 +0200

    create views and urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path, include
-from blog import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('about/',views.AboutView.as_view(),name='about'),
-]
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+    ]
