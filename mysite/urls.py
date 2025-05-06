59d8f6b83451cdbcf1d91eccd3e21644666167fd
ARYAN MASSOUDY <116915753+aryan-massoudy@users.noreply.github.com>
2025-05-06 14:37:28 +0200

    Fixed Logout functionality

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView 
+from django.urls import reverse_lazy
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
     path('accounts/login/', LoginView.as_view(), name='login'),
-    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/logout/', LogoutView.as_view(next_page=reverse_lazy('post_list')), name='logout'),
 ]
